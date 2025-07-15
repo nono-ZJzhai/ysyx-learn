da3c1e22aac5d7f88771bec2ee1880de2a473297
nono-ZJzhai <2514525388@qq.com>
2025-07-15 02:20:49 -0400

    OK

---
+++ ysyx-learn/nvboard/example/build/obj_dir/auto_bind.d
@@ -0,0 +1,9 @@
+auto_bind.o: \
+ /home/nonozhai/ysyx-workbench/nvboard/example/build/auto_bind.cpp \
+ /home/nonozhai/ysyx-workbench/nvboard/usr/include/nvboard.h \
+ /home/nonozhai/ysyx-workbench/nvboard/usr/include/pins.h Vtop.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h
---
+++ ysyx-learn/nvboard/example/build/obj_dir/main.d
@@ -0,0 +1,8 @@
+main.o: /home/nonozhai/ysyx-workbench/nvboard/example/csrc/main.cpp \
+ /home/nonozhai/ysyx-workbench/nvboard/usr/include/nvboard.h \
+ /home/nonozhai/ysyx-workbench/nvboard/usr/include/pins.h Vtop.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h
